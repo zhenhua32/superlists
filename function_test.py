4a9eaea15d6eb081593f8d3235d015647e451d5d
zhenhua32 <zhenhua32@gmail.com>
2017-02-21 10:44:59 +0800

    update

--- function_test.py
+++ function_test.py
@@ -16,7 +16,7 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 打开首页
         self.browser.get('http://localhost:8000')
-        time.sleep(5)
+        time.sleep(3)
 
         # 首页有 'To-Do' 这个词
         self.assertIn('To-Do', self.browser.title)
@@ -39,20 +39,23 @@ class NewVistorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy an apple' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy an apple', [row.text for row in rows])
 
         # 页面上又显示了一个文本框, 可以再次输入待办事项
         # 再次输入 ' Buy a car'
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy a car')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面更新, 显示了两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy an apple', [row.text for row in rows])
+        self.assertIn('2: Buy a car', [row.text for row in rows])
 
         # 网站生成了一个唯一的 URL
         # 页面中附带这个功能的说明
-
+        self.fail('Finish the test')
         # 访问这个 URL, 代办事项还在
 
 
